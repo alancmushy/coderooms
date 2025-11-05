10a310e03003ffa97a75a5e7c941304af368b4b6
Alan Mushyirahamwe <alancmushy@gmail.com>
2025-11-04 23:58:33 -0500

    Fixing path

--- client/api.js
+++ client/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LANGUAGES } from "./elements/codebox";
+import { LANGUAGES } from "./elements/CodeBox";
 const API = axios.create({
    baseURL:"https://emkc.org/api/v2/piston"
 })
